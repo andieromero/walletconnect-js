9264331c0647137bcb38a40568632b768ff68879
andieromero <andriana@beltranfamily.com>
2023-02-02 11:40:15 -0800

    update connect url

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -25,5 +25,6 @@ export const APP_STORE_APPLE_FIGURE =
 
 // FIGURE WEB WALLET URLS
 export const FIGURE_WEB_WALLET_TEST_URL =
-  'http://localhost:3001/figure-wallet/connect';
-export const FIGURE_WEB_WALLET_PROD_URL = 'https://www.figure.com/figure-wallet';
+  'https://test.figure.com/figure-wallet/connect';
+export const FIGURE_WEB_WALLET_PROD_URL =
+  'https://www.figure.com/figure-wallet/connect';
